c07b95f10e5b10ddb4a195768bc3f69925172153
am-mussy <57048090+am-mussy@users.noreply.github.com>
2020-04-30 23:49:29 +0300

    master

--- w.js
+++ w.js
@@ -1,57 +1,57 @@
-define([], function() {
-
+define([], function () {
   return {
-      onSave : function() {
-         console.log('external on save')
-      },
-      settings: (self) => {
-        w_code = "asdasd";
-        m_data = [
-          {
-            option: "option1",
-            id: "id1",
-          },
-          {
-            option: "option2",
-            id: "id2",
-          },
-          {
-            option: "option3",
-            id: "id3",
-          },
-        ]; //массив д
+    onSave: function () {
+      console.log("external on save");
+    },
+    settings: (self) => {
+      w_code = "asdasd";
+      m_data = [
+        {
+          option: "option1",
+          id: "id1",
+        },
+        {
+          option: "option2",
+          id: "id2",
+        },
+        {
+          option: "option3",
+          id: "id3",
+        },
+      ]; //массив д
 
-        var data = self.render(
-          { ref: "/tmpl/controls/select.twig" },
-          {
-            arr: m_data,
-            class_name: "subs_w",
-            id: w_code + "_list",
-          }
-        );
+      var data = self.render(
+        { ref: "/tmpl/controls/select.twig" },
+        {
+          arr: m_data,
+          class_name: "subs_w",
+          id: w_code + "_list",
+        }
+      );
 
-        $(".widget_settings_block__descr").after(
-          `
+      $(".widget_settings_block__descr").after(
+        `
           <div class="widget_settings_block__item_field" id="users">
           <br>
+          <p>Проверка, работы виджета, обновленя и все такое</p>
           <p>ID Группы пользователей:</p>
           <input name="group" id="group" class="inputGroupID" type="number" placeholder="id group" />
           </div>
           `
-        );
-        $(".widget_settings_block__descr").after(data);
-        $(".inputGroupID").val($("input[name = idgroup]").val());
+      );
+      $(".widget_settings_block__descr").after(data);
+      $(".inputGroupID").val($("input[name = idgroup]").val());
 
-        let inputGroupID;
-        $(".inputGroupID").change(function () {
-          console.log("change");
-          inputGroupID = $(".inputGroupID").val();
-          inputGroupID = Number.parseInt(inputGroupID);
-          $("input[name = idgroup]").val(inputGroupID);
-        });
+      let inputGroupID;
+      $(".inputGroupID").change(function () {
+        console.log("change");
+        inputGroupID = $(".inputGroupID").val();
+        inputGroupID = Number.parseInt(inputGroupID);
+        $("input[name = idgroup]").val(inputGroupID);
+      });
 
-        //$(".inputGroupID").val($("input[name = idgroup]").val());
-        $('input[name="group"]').trigger("change");
-      } 
+      //$(".inputGroupID").val($("input[name = idgroup]").val());
+      $('input[name="group"]').trigger("change");
+    },
   };
 });
