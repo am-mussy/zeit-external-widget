535cfc6d79b73bbab50c1fe599f9cd3053885b53
am-mussy <57048090+am-mussy@users.noreply.github.com>
2020-05-03 16:27:27 +0300

    m

--- w.js
+++ w.js
@@ -70,7 +70,7 @@ define([], function () {
         { ref: "/tmpl/controls/multiselect.twig" },
         {
           values: arr,
-          name: "milti",
+          name: "mislti",
         }
       );
       $(".widget_settings_block__descr").append("<br>" + data + "<br>");
